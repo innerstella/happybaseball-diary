6c4860a4047fb2d467f7964235959f2687825d14
innerstella <suji0509@naver.com>
2024-04-28 16:21:16 +0900

    feat: recoil-persist

--- src/recoil/system.ts
+++ src/recoil/system.ts
@@ -1,16 +1,22 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const currSeasonState = atom({
   key: "currSeasonState",
   default: 2024,
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const loginState = atom({
   key: "loginState",
   default: false,
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const userDataState = atom({
   key: "userDataState",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
