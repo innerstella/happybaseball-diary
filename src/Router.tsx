abc1864c83136a0001cb5fd2780845167f3772c8
innerstella <suji0509@naver.com>
2024-05-08 15:17:26 +0900

    chore: react-query 초기 세팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import HomePage from "./pages/home";
 import CreatePage from "./pages/create/Create";
 import MyPage from "./pages/mypage";
@@ -7,17 +10,22 @@ import GlobalStyles from "./GlobalStyles";
 import WeatherPage from "./pages/weather";
 
 const AppRouter = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/detail/:id" element={<DetailPage />} />
-        <Route path="/weather" element={<WeatherPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+          <Route path="/weather" element={<WeatherPage />} />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
